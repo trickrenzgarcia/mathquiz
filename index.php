6d9269d9874822bd22b6f918607356cd3483715f
Patrick Renz <trickrenzgarcia@gmail.com>
2021-05-07 07:49:21 +0800

    update

--- index.php
+++ index.php
@@ -9,14 +9,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="static/images/logo.png">
+  <link rel="stylesheet" type="text/css" href="static/css/materialize.min.css">
+  <link rel="stylesheet" type="text/css" href="static/css/index.css">
+  <link rel="icon" href="static/images/logo.jpg">
   <title>Math Quiz Game</title>
 </head>
 <body>
-  <?php if(isset($_SESSION['UserLogin'])){?>
-    <a href="logout.php">Logout</a>
-  <?php } else { ?>
-    <a href="login.php">Login</a>
-  <?php }?>
+  <nav class="blue lighten-1">
+    <div class="nav-wrapper">
+      <div class="nav-container">
+        <img src="static/images/logo.png" alt="logo" width="100px">
+        <a href="index.php" class="brand-logo hide-on-med-and-down">The Math Quiz Game</a>
+        <ul id="nav-mobile" class="right ">
+          <?php if(isset($_SESSION['UserLogin'])){?>
+            <li><a href="logout.php">Logout</a></li>
+          <?php } else { ?>
+            <li><a href="login.php">LOGIN</a></li>
+            <li><a href="signup.php">SIGN UP</a></li>
+          <?php }?>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  
 </body>
-</html>l
+</html>
