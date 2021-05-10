95e50c2dc8a9636164fa645225424078e4b30f75
Patrick Renz <trickrenzgarcia@gmail.com>
2021-05-11 07:44:40 +0800

    up

--- signup.php
+++ signup.php
@@ -30,29 +30,29 @@
               <div class="row">
                 <div class="input-field col s6 l6 m6">
                   <label for="first_name">First Name</label><br>
-                  <input placeholder="First Name" type="text" name="first_name" id="first_name" class="validate"/>
+                  <input type="text" name="first_name" id="first_name" class="validate" required/>
                 </div>
                 <div class="input-field col s6 l6 m6">
                   <label for="last_name">Last Name</label><br>
-                  <input type="text" name="last_name" id="last_name" class="validate"/>
+                  <input type="text" name="last_name" id="last_name" class="validate" required/>
                 </div>
               </div>
               <div class="row">
                 <div class="input-field col s12">
                   <label>Email</label><br>
-                  <input type="email" name="email" id="email" class="validate"/>
+                  <input type="email" name="email" id="email" class="validate" required/>
                 </div>
               </div>
               <div class="row">
                 <div class="input-field col s12"> 
                   <label for="password">Password</label><br>
-                  <input type="password" name="password_1" id="password_1" class="validate"/>
+                  <input type="password" name="password_1" id="password_1" class="validate" required/>
                 </div>
               </div>
               <div class="row">
                 <div class="input-field col s12">
                   <label for="password">Confirm Password</label><br>
-                  <input type="password" name="password_2" id="password_2" class="validate"/>
+                  <input type="password" name="password_2" id="password_2" class="validate" required/>
                 </div>
               </div>
               <button class="button_submit" type="submit" name="signup">Sign up</button>
